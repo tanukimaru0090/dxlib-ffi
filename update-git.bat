dbc4583971154ac97124cc528b25eacbdf85afb2
tanukimaru0090 <TANUSAN1015@gmail.com>
2023-10-02 01:17:25 +0900

    Update

--- update-git.bat
+++ update-git.bat
@@ -1,5 +1,13 @@
 @echo off
 REM cargo check 
 cargo check
-REM git command
+
+
+REM Cargo Checkが成功したら
+if %errorlevel% equ 0 (
 git add . && git commit -m Update && git push 
+)
+REM それ以外は何もしない
+else(
+
+)
