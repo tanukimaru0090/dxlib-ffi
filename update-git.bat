db770d767b6603db60d4421cb540d3c2286b5448
tanukimaru0090 <TANUSAN1015@gmail.com>
2023-10-02 01:31:50 +0900

    Update

--- update-git.bat
+++ update-git.bat
@@ -1,9 +1,15 @@
-@echo off
 REM cargo check 
-cargo check
+@cargo check
 REM Cargo Checkが成功したら
 if %errorlevel% equ 0 (
-REM git command
-git add . && git commit -m Update && git push 
+	@git add . && git commit -m Update && git push 
+	REM git コマンドが失敗したら
+	REM index.lockを削除してもう一度実行
+	if %errorlevel% equ 1 (
+		@rm "./.git/index.lock" 
+		echo index.lockを削除しました
+		echo 再度git commandを試します。
+		@git add . && git commit -m Update && git push 
+	)
 )
 
